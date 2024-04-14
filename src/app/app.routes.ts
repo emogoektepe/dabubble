ebc62de061ab1aff5cfa1e178363d4fdbafa88d2
Emre Göktepe <146372845+emogoektepe@users.noreply.github.com>
2024-04-14 23:08:33 +0200

    Routing and main page done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { CreateAccountComponent } from './main/create-account/create-account.component';
+import { AvatarComponent } from './main/avatar/avatar.component';
+import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      {
+        path: '',
+        component: LoginComponent,
+      },
+      {
+        path: 'create-account',
+        component: CreateAccountComponent,
+      },
+      {
+        path: 'avatar',
+        component: AvatarComponent,
+      },
+    ],
+  },
+];
