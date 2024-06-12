8aa5e4aa9d0ad5d2d65f26d8ff4d97d076366fd7
Emre Göktepe <146372845+emogoektepe@users.noreply.github.com>
2024-06-12 18:52:05 +0200

    create account form

--- src/app/main/create-account/create-account.component.ts
+++ src/app/main/create-account/create-account.component.ts
@@ -1,10 +1,18 @@
+import { CommonModule, Location } from '@angular/common';
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { Location } from '@angular/common';
+import {
+  AbstractControl,
+  FormBuilder,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-create-account',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule, CommonModule],
   templateUrl: './create-account.component.html',
   styleUrl: './create-account.component.scss',
 })
@@ -12,8 +20,43 @@ export class CreateAccountComponent {
   @ViewChild('nameInput') nameInputRef!: ElementRef;
   @ViewChild('mailInput') mailInputRef!: ElementRef;
   @ViewChild('passwortInput') passwortInputtRef!: ElementRef;
+  @ViewChild('policyCheckbox') policyCheckbox!: ElementRef;
 
-  constructor(private location: Location) {}
+  isSubmitted = false;
+
+  registerForm = this.fb.group({
+    fullName: ['', [Validators.required, this.noLeadingWhitespaceValidator]],
+    email: [
+      '',
+      [
+        Validators.required,
+        this.noLeadingWhitespaceValidator,
+        Validators.email,
+      ],
+    ],
+    password: ['', [Validators.required, this.noLeadingWhitespaceValidator]],
+    checkbox: ['', Validators.requiredTrue],
+  });
+
+  constructor(private location: Location, private fb: FormBuilder) {}
+
+  onSubmit(): void {
+    console.log(
+      'submitted form',
+      this.registerForm.value,
+      this.registerForm.invalid
+    );
+    this.isSubmitted = true;
+  }
+
+  noLeadingWhitespaceValidator(
+    control: AbstractControl
+  ): ValidationErrors | null {
+    if (control.value && control.value.startsWith(' ')) {
+      return { leadingWhitespace: true };
+    }
+    return null;
+  }
 
   focusNameInput() {
     this.nameInputRef.nativeElement.focus();
