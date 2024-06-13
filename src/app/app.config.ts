be27c5d4ba8a4ffed2ea2cce592f3ef54346e337
Emre Göktepe <146372845+emogoektepe@users.noreply.github.com>
2024-06-13 20:00:09 +0200

    dependency update and firebase api

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,27 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), importProvidersFrom(provideFirebaseApp(() => initializeApp({"projectId":"dabubble-3ffad","appId":"1:41021622700:web:01cd710e735ee1b2a65209","storageBucket":"dabubble-3ffad.appspot.com","apiKey":"AIzaSyA85XBxthQeX9fRr-Z_KTDKJ8WeGM-njNA","authDomain":"dabubble-3ffad.firebaseapp.com","messagingSenderId":"41021622700"}))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore()))]
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(
+      () =>
+        initializeApp({
+          projectId: 'dabubble-3ffad',
+          appId: '1:41021622700:web:01cd710e735ee1b2a65209',
+          storageBucket: 'dabubble-3ffad.appspot.com',
+          apiKey: 'AIzaSyA85XBxthQeX9fRr-Z_KTDKJ8WeGM-njNA',
+          authDomain: 'dabubble-3ffad.firebaseapp.com',
+          messagingSenderId: '41021622700',
+        }),
+      provideAuth(() => getAuth()),
+      provideFirestore(() => getFirestore()),
+      provideAnimations()
+    ),
+  ],
 };
